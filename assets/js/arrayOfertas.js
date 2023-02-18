d91d627ae56f6c7a4239ec3b2f6879c39b0ac1aa
Pololokka <vitorbulbovas1@hotmail.com>
2023-02-18 14:18:50 -0300

    :sparkles: feat:função de adicionar produtos concluída

--- assets/js/arrayOfertas.js
+++ assets/js/arrayOfertas.js
@@ -1,4 +1,4 @@
-const produtosOferta = [
+const produtosOferta =  JSON.parse(localStorage.getItem('arrayOfertas')) || [
     {
         id : "0",
         imagem : "https://cdn.discordapp.com/attachments/621499803884584998/1076243333996875867/sapo.png",
@@ -21,53 +21,12 @@ const produtosOferta = [
         nome : "Capitão Rex",
         descricao: "O comandante da 501ª está pronto para liderar suas tropas. Mini de plástico pintada a mão.",
         preco: "120,00"
-    },
-    {
-        id : "3",
-        imagem : "https://cdn.discordapp.com/attachments/621499803884584998/1076243333996875867/sapo.png",
-        alt: "Sapo, de Chrono Trigger",
-        nome : "Sapola do Chrono Trigger",
-        descricao: "O mais valente dos cavaleiros, agora na sua casa! Impresso em resina e pintado a mão.",
-        preco: "150,00"
-    },
-    {
-        id : "4",
-        imagem : "https://cdn.discordapp.com/attachments/621499803884584998/1076243333749416047/rolf.png",
-        alt: "Rolf, o Bárbaro",
-        nome : "Rolf, o Bárbaro",
-        descricao: "Este sanguinário bárbaro mata orcs zumbis no café da manhã. Mini de plástico pintada a mão.",
-        preco: "100,00"
-    },
-    {
-        id : "5",
-        imagem : "https://cdn.discordapp.com/attachments/621499803884584998/1076243333531324576/rex.png",
-        alt: "Capitão Rex",
-        nome : "Capitão Rex",
-        descricao: "O comandante da 501ª está pronto para liderar suas tropas. Mini de plástico pintada a mão.",
-        preco: "120,00"
-    },
-    {
-        id : "6",
-        imagem : "https://cdn.discordapp.com/attachments/621499803884584998/1076243333996875867/sapo.png",
-        alt: "Sapo, de Chrono Trigger",
-        nome : "Sapola do Chrono Trigger",
-        descricao: "O mais valente dos cavaleiros, agora na sua casa! Impresso em resina e pintado a mão.",
-        preco: "150,00"
-    },
-    {
-        id : "7",
-        imagem : "https://cdn.discordapp.com/attachments/621499803884584998/1076243333749416047/rolf.png",
-        alt: "Rolf, o Bárbaro",
-        nome : "Rolf, o Bárbaro",
-        descricao: "Este sanguinário bárbaro mata orcs zumbis no café da manhã. Mini de plástico pintada a mão.",
-        preco: "100,00"
-    },
-    {
-        id : "8",
-        imagem : "https://cdn.discordapp.com/attachments/621499803884584998/1076243333531324576/rex.png",
-        alt: "Capitão Rex",
-        nome : "Capitão Rex",
-        descricao: "O comandante da 501ª está pronto para liderar suas tropas. Mini de plástico pintada a mão.",
-        preco: "120,00"
     }
- ];
+];
+
+function salvaOfertas() {
+    const arrayOfertasJSON = JSON.stringify(produtosOferta);
+    localStorage.setItem('arrayOfertas', arrayOfertasJSON);
+}
+
+salvaOfertas();
--- assets/js/arrayProdutos.js
+++ assets/js/arrayProdutos.js
@@ -1,4 +1,4 @@
-const produtosTodos = [
+const produtosTodos = JSON.parse(localStorage.getItem('arrayProdutos')) || [
     {
         id : "0",
         imagem : "https://cdn.discordapp.com/attachments/621499803884584998/1076243333996875867/sapo.png",
@@ -22,53 +22,12 @@ const produtosTodos = [
         nome : "Capitão Rex",
         descricao: "O comandante da 501ª está pronto para liderar suas tropas. Mini de plástico pintada a mão.",
         preco: "120,00"
-    },
-    {
-        id : "3",
-        imagem : "https://cdn.discordapp.com/attachments/621499803884584998/1076243333996875867/sapo.png",
-        alt: "Sapo, de Chrono Trigger",
-        nome : "Sapola do Chrono Trigger",
-        descricao: "O mais valente dos cavaleiros, agora na sua casa! Impresso em resina e pintado a mão.",
-        preco: "150,00"
-    },
-    {
-        id : "4",
-        imagem : "https://cdn.discordapp.com/attachments/621499803884584998/1076243333749416047/rolf.png",
-        alt: "Rolf, o Bárbaro",
-        nome : "Rolf, o Bárbaro",
-        descricao: "Este sanguinário bárbaro mata orcs zumbis no café da manhã. Mini de plástico pintada a mão.",
-        preco: "100,00"
-    },
-    {
-        id : "5",
-        imagem : "https://cdn.discordapp.com/attachments/621499803884584998/1076243333531324576/rex.png",
-        alt: "Capitão Rex",
-        nome : "Capitão Rex",
-        descricao: "O comandante da 501ª está pronto para liderar suas tropas. Mini de plástico pintada a mão.",
-        preco: "120,00"
-    },
-    {
-        id : "6",
-        imagem : "https://cdn.discordapp.com/attachments/621499803884584998/1076243333996875867/sapo.png",
-        alt: "Sapo, de Chrono Trigger",
-        nome : "Sapola do Chrono Trigger",
-        descricao: "O mais valente dos cavaleiros, agora na sua casa! Impresso em resina e pintado a mão.",
-        preco: "150,00"
-    },
-    {
-        id : "7",
-        imagem : "https://cdn.discordapp.com/attachments/621499803884584998/1076243333749416047/rolf.png",
-        alt: "Rolf, o Bárbaro",
-        nome : "Rolf, o Bárbaro",
-        descricao: "Este sanguinário bárbaro mata orcs zumbis no café da manhã. Mini de plástico pintada a mão.",
-        preco: "100,00"
-    },
-    {
-        id : "22",
-        imagem : "https://cdn.discordapp.com/attachments/621499803884584998/1076243333531324576/rex.png",
-        alt: "Capitão Rex",
-        nome : "Capitão Rex",
-        descricao: "O comandante da 501ª está pronto para liderar suas tropas. Mini de plástico pintada a mão.",
-        preco: "120,00"
     }
- ];
+];
+
+function salvaProdutos() {
+    const arrayProdutosJSON = JSON.stringify(produtosTodos);
+    localStorage.setItem('arrayProdutos', arrayProdutosJSON);
+}
+
+salvaProdutos();
